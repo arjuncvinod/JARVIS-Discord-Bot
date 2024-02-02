9dd82855fb94b8287b1ec1b31bea0b0051320f43
Arjun C Vinod <arjuncvinod007@gmail.com>
2024-02-02 12:08:29 +0530

    added gpt-3.5-turbo

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import discord
 import decouple 
 from discord.ext import commands
+import openai
 
 jarvis=commands.Bot(command_prefix="/",intents=discord.Intents.all())
 
+def generate_app_ideas(prompt):
+    openai.api_key = decouple.config('OPENAI_API_KEY')
 
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    return completion.choices[0].message.content
+
+
+def trimmsg(text):
+    return text[:1000]
 @jarvis.event
 
 async def on_member_join(member):
@@ -19,7 +32,7 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author.bot:
         return
-    # await message.reply("Roger that!")
+    await message.reply(trimmsg(generate_app_ideas(message.content)))
     await message.add_reaction("❤️")
 
 
